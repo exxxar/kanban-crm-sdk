83388f83f3ee20f2b378bb32c48f8b5f679d969d
exxxar <exxxar@gmail.com>
2026-02-28 20:09:56 +0300

    update

--- src/Services/KanbanClient.php
+++ src/Services/KanbanClient.php
@@ -8,15 +8,30 @@ class KanbanClient
 {
     protected Client $http;
 
+    protected string $token;
+    protected string $baseUrl;
+
     public function __construct($baseUrl, $token = null)
     {
+        $this->baseUrl = $baseUrl;
+        $this->token = $token;
+    }
+
+    protected function prepareClient(){
+
         $this->http = new Client([
-            'base_uri' => $baseUrl,
+            'base_uri' => $this->baseUrl,
             'headers' => [
                 'Accept' => 'application/json',
-                'Authorization' => $token ? "Bearer $token" : null,
+                'Authorization' => $this->token ? "Bearer $this->token" : null,
             ]
         ]);
+    }
+
+    public function setToken($token){
+
+        $this->token = $token;
+        return $this;
     }
 
     public function client(): static
@@ -26,21 +41,25 @@ class KanbanClient
 
     public function tasks()
     {
+        $this->prepareClient();
         return new Tasks($this->http);
     }
 
     public function comments()
     {
+        $this->prepareClient();
         return new Comments($this->http);
     }
 
     public function attachments()
     {
+        $this->prepareClient();
         return new Attachments($this->http);
     }
 
     public function boards()
     {
+        $this->prepareClient();
         return new Boards($this->http);
     }
 }
